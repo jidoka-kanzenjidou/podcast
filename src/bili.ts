9c35c5b869fde3f8d6905b73acac3bce625801ac
Your Name <you@example.com>
2025-03-30 10:39:06 +0200

    Add

--- src/bili.ts
+++ src/bili.ts
@@ -3,7 +3,5 @@ import { PodcastVideoProcessor } from "./PodcastVideoProcessor.js";
 (async function () {
     const processor = new PodcastVideoProcessor();
     const finalOutputPath = await processor.processPodcastToVideo('Hãy tạo một podcast về công nghệ nano.')
-    console.log({
-        finalOutputPath,
-    })
+    console.log(finalOutputPath)
 })();
